b323f635790ef808de4f62017deeae1f89d23c8a
GiackAloZ <giacomo.aloisi1998@gmail.com>
2020-08-19 19:30:13 +0200

    Change while condition and add break cause

--- src/mpi-convex-hull.c
+++ src/mpi-convex-hull.c
@@ -323,7 +323,12 @@ void convex_hull(const points_t *pset, points_t *hull, int rank, int n_procs)
 
         printf("local_cur = %f %f\n", local_cur.x, local_cur.y);
         printf("local_leftmost = %f %f\n", local_leftmost.x, local_leftmost.y);
-    } while (local_cur.x != local_leftmost.x && local_cur.y != local_leftmost.y);
+
+        if (local_cur.x == local_leftmost.x && local_cur.y == local_leftmost.y) {
+            printf("done!\n");
+            break;
+        }
+    } while (1);
 
     free(local_p);
 
