fd61c967e6d0e935e5b4b5c5d5b3d959a93c6216
GiackAloZ <giacomo.aloisi1998@gmail.com>
2020-08-20 10:50:08 +0200

    Add n_procs in main

--- src/mpi-convex-hull-split-4.c
+++ src/mpi-convex-hull-split-4.c
@@ -485,9 +485,10 @@ int main( int argc, char *argv[]  )
     points_t pset, hull;
     double tstart, elapsed;
 
-    int rank;
+    int rank, n_procs;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &n_procs);
     
     if (rank == 0){
         read_input(stdin, &pset);
